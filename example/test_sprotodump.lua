98fe68b7052c35ba8ba5e80916a79144f0966e1f
TD1900 <samoyedsun@gmail.com>
2018-02-22 08:58:31 +0800

    删除没用的

--- example/test_sprotodump.lua
+++ example/test_sprotodump.lua
@@ -6,7 +6,3 @@ local dump = sprotodump()
 dump:set_dump_path(root.."/proto/")
 dump:load(root.."/proto/", true)
 dump:dump("protocol.spb")
-
-
-dump:set_dump_file("protocol.txt")
-dump:dump("protocol.spb", true)
