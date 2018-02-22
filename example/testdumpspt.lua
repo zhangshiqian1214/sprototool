fcaa797d182fd316b4889d8a74528da822fc4ebd
TD1900 <samoyedsun@gmail.com>
2018-02-22 09:28:57 +0800

    删除没用的

--- example/testdumpspt.lua
+++ example/testdumpspt.lua
@@ -1,8 +1,7 @@
 require "common"
 
 local sprotodump = require "sprotodump"
-
 local dump = sprotodump()
 dump:set_dump_path(root.."/proto/")
 dump:load(root.."/proto/", true)
-dump:dump("protocol.spb")
+dump:dump("protocol.spt")
